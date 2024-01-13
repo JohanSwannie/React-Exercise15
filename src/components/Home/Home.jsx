f8d328b33cc7d178415cbb1e54b2b0bfe6f0633a
Johan Swan <jswannieman@gmail.com>
2024-01-14 09:57:55 +1300

    Update Home JSX Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
             </h5>
 
             <Link
-              className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
+              className="inline-flex text-white items-center px-6 py-2 font-small bg-orange-700 rounded-lg hover:opacity-75"
               to="/"
             >
               &nbsp; Download now
@@ -20,22 +20,14 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="absolute inset-0 w-full sm:my-10 sm:pt-1 pt-12 h-full ">
+        <div className="absolute inset-0 w-full sm:my-8 sm:pt-1 pt-12 h-full ">
           <img
-            className="w-60"
+            className="w-72"
             src="https://img.freepik.com/premium-vector/utch-man-viewed-from-side-behind-laptop-02-copy-5-01_961307-1185.jpg"
             alt="image1"
           />
         </div>
       </aside>
-
-      <div className="grid  place-items-center sm:mt-20">
-        <img
-          className="sm:w-96 w-48"
-          src="https://www.joberty.com/blog/content/images/2023/09/senior-software-engineer.png"
-          alt="image2"
-        />
-      </div>
     </div>
   );
 }

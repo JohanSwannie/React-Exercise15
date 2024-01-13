b52da5f592d2655eda0bd5a761d2f0441a9e0d95
Johan Swan <jswannieman@gmail.com>
2024-01-14 12:19:27 +1300

    Update About JSX Component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,17 +4,18 @@ import Scroll from "../Scroll/Scroll";
 export default function About() {
   Scroll();
   return (
-    <div className="py-16 bg-white">
-      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-          <div className="md:5/12 lg:w-5/12">
+    <div className="py-8 bg-white">
+      <div className="container m-auto px-2 text-gray-600 md:px-12 xl:px-6">
+        <div className="space-y-3 md:space-y-0 md:flex md:gap-1 lg:items-center lg:gap-2">
+          <div className="md:1/2 lg:w-1/2">
             <img
               src="https://ares.decipherzone.com/blog-manager/uploads/banner_webp_da06d145-93f9-4df9-8c7e-1e2c332c3a4a.webp"
+              width="300"
               alt="image"
             />
           </div>
           <div className="md:7/12 lg:w-6/12">
-            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+            <h2 className="text-lg text-gray-900 font-bold md:text-4xl">
               React development is carried out by passionate developers
             </h2>
             <p className="mt-6 text-gray-600">

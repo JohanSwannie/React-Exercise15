dd5aa32db6233b096029551cb8de9f1f490ebaa8
Johan Swan <jswannieman@gmail.com>
2024-01-13 21:52:47 +1300

    Update Home JSX Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,10 +7,7 @@ export default function Home() {
       <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
         <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
           <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-            <h2 className="text-4xl font-bold sm:text-5xl">
-              Download Now
-              <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
-            </h2>
+            <h2 className="text-4xl font-bold sm:text-5xl">Read More</h2>
 
             <Link
               className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
@@ -34,7 +31,7 @@ export default function Home() {
         <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
           <img
             className="w-96"
-            src="https://i.ibb.co/5BCcDYB/Remote2.png"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGd8ymjLUk_u-VpflL-pyX2nXvvHBLjVZk3w&usqp=CAU"
             alt="image1"
           />
         </div>

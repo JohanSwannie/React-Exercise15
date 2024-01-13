f8436131b7956137f9f1a3d6fa32401cdc68b573
Johan Swan <jswannieman@gmail.com>
2024-01-14 12:34:18 +1300

    Update Header JSX Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,13 +16,13 @@ export default function Header() {
           <div className="flex items-center lg:order-2">
             <Link
               to="#"
-              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Log in
             </Link>
             <Link
               to="#"
-              className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Get started
             </Link>
@@ -31,7 +31,7 @@ export default function Header() {
             className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <ul className="flex flex-col mt-4 font-semibold text-lg lg:flex-row lg:space-x-20 lg:mt-0">
               <li>
                 <NavLink
                   to="/"

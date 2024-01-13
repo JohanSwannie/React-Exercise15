d73d739c45511167f23dacb70e2d53f155e4c001
Johan Swan <jswannieman@gmail.com>
2024-01-14 09:57:05 +1300

    Update main JSX Component

--- src/main.jsx
+++ src/main.jsx
@@ -10,14 +10,16 @@ import {
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 import User from "./components/User/User.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
       <Route path="user/" element={<User />}>
-        <Route path="about" element={<About />} />
         <Route path=":userid" element={<User />} />
       </Route>
     </Route>

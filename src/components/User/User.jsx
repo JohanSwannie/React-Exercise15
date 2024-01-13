31ca6b81ceacdb902f591901e3998ad3d93e950f
Johan Swan <jswannieman@gmail.com>
2024-01-14 09:58:06 +1300

    Update User JSX Component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 function User() {
   const { userid } = useParams();
   return (
-    <div className="bg-orange-500 text-black text-3xl text-center py-5">
+    <div className="bg-orange-200 text-black text-3xl text-center py-5">
       User: {userid}
     </div>
   );

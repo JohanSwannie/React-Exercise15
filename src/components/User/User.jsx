747e294b8095cb36e61a353fd7eeab3cd7cb1bf0
Johan Swan <jswannieman@gmail.com>
2024-01-14 12:04:47 +1300

    Update User JSX Component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,8 +1,11 @@
-import React from "react";
 import { useParams } from "react-router-dom";
+import Scroll from "../Scroll/Scroll";
 
 function User() {
   const { userid } = useParams();
+
+  Scroll();
+
   return (
     <div className="bg-orange-200 text-black text-3xl text-center py-5">
       User: {userid}

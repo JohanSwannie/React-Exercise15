e5f1e1ce80e008b75eb16509e6ddf352aaf81c17
Johan Swan <jswannieman@gmail.com>
2024-01-14 09:57:20 +1300

    Commit Contact JSX Component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl mt-7 mb-7 underline">Contact</h1>
+      <p className="mb-5">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        fugiat amet quaerat nostrum perspiciatis non sed exercitationem
+        provident beatae sequi? Facilis, praesentium? Dicta quibusdam debitis,
+        earum illum recusandae voluptatum commodi fugiat consectetur optio
+        laboriosam quas delectus. Ullam ea, culpa omnis est deleniti facilis
+        quae debitis consequatur error expedita iusto assumenda harum a
+        necessitatibus earum voluptatibus inventore fugiat dolore eligendi
+        molestiae eum modi similique maiores! Necessitatibus quo quod est
+        eligendi ullam optio itaque voluptate veniam nostrum saepe, quam tempore
+        vero reprehenderit voluptatem esse fugiat nemo accusantium consectetur
+        ratione nulla architecto nesciunt voluptatibus obcaecati. Voluptates
+        aliquid ea assumenda consectetur incidunt eaque vero deserunt neque
+        praesentium debitis! Tempora consequuntur consectetur saepe quasi
+        asperiores iste itaque culpa, amet, tenetur, error eos quod harum.
+      </p>
+      <p className="mb-5">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis
+        necessitatibus adipisci tempora autem sit temporibus, explicabo
+        excepturi officia nesciunt repellat veritatis, consequatur veniam
+        deserunt nulla porro deleniti. Repellat accusamus, ullam vel harum sed,
+        corporis tempora maxime assumenda natus iste cupiditate, perspiciatis
+        blanditiis iure voluptas esse consectetur minus voluptatibus voluptatum?
+        Ab molestiae doloribus soluta dicta amet officiis quibusdam, labore
+        temporibus. Provident obcaecati tempora corrupti odit error
+        necessitatibus sint nisi saepe, itaque facilis. Laudantium fugiat illo
+        provident blanditiis expedita, odit delectus! Asperiores harum
+        reprehenderit delectus impedit soluta facere. Ipsa aliquid dolore nisi.
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
